d61a99a75d6b2919b3f3d05f41b3258b5308f617
ddii596 <70442039+ddii596@users.noreply.github.com>
2024-10-12 11:15:57 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
+
+ resource "aws_instance" "app_server" {
+   ami           = "ami-08d70e59c07c61a3a"
+   instance_type = "t2.micro"
+
+   tags = {
+    Name = var.instance_name
+   }
+ }
