478a0fb2ff998883241f574a0acbb67d093d7526
ddii596 <adisonph@gmail.com>
2024-10-14 19:52:30 -0300

    testando site statico

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,15 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  acl    = "public-read" 
+  acl    = "public-read"
 
   website {
     index_document = "index.html"      # Documento principal do site
     error_document = "error.html"      # Documento de erro (opcional)
-}
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.static_site.id
+  bucket = aws_s3_bucket.bucket.id  # Correção no nome do recurso "bucket"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -17,17 +17,16 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       Effect    = "Allow"
       Principal = "*"
       Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      Resource  = "${aws_s3_bucket.bucket.arn}/*"  # Correção no nome do recurso "bucket"
     }]
   })
 }
 
-}
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fff1b9a61dec8a5f"
+  instance_type = "t2.micro"
 
- resource "aws_instance" "app_server" {
-   ami           = "ami-0fff1b9a61dec8a5f"
-   instance_type = "t2.micro"
-   tags = {
+  tags = {
     Name = var.instance_name
-   }
- }
+  }
+}
