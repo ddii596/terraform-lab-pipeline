67646e76ae9b3260662f42c768d5c66d7e4227b4
ddii596 <adisonph@gmail.com>
2024-10-14 19:36:08 -0300

    testando site statico

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,23 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+  acl    = "public-read" 
+
+  website {
+    index_document = "index.html"      # Documento principal do site
+    error_document = "error.html"      # Documento de erro (opcional)
+}
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.static_site.arn}/*"
+    }]
+  })
 }
