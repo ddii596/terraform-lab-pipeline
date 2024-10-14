1f54715d1bf43b27173bc4fc194a56c5944128c7
ddii596 <adisonph@gmail.com>
2024-10-14 20:07:28 -0300

    testando site statico

--- infra/main.tf
+++ infra/main.tf
@@ -1,31 +1,48 @@
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = false  # Desativa o bloqueio de ACLs públicas
+  block_public_policy     = false  # Desativa o bloqueio de políticas públicas
+  ignore_public_acls      = false  # Permite ACLs públicas
+  restrict_public_buckets = false  # Permite políticas públicas no bucket
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 
   website {
-    index_document = "index.html"      # Documento principal do site
-    error_document = "error.html"      # Documento de erro (opcional)
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = {
+    Name        = "Static Website Bucket"
+    Environment = "Production"
   }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id  # Correção no nome do recurso "bucket"
+  bucket = aws_s3_bucket.bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.bucket.arn}/*"  # Correção no nome do recurso "bucket"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.bucket.arn}/index.html"  # Permitir acesso apenas ao index.html
+      },
+      {
+        Effect = "Deny"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.bucket.arn}/*"
+        Condition = {
+          StringNotEquals = {
+            "s3:prefix" = "index.html"
+          }
+        }
+      }
+    ]
   })
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-0fff1b9a61dec8a5f"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
-  }
 }
