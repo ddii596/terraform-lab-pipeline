9fab7f84b53d2c971f76c4192e8a4387050f7ef6
ddii596 <adisonph@gmail.com>
2024-10-16 16:44:12 -0300

    testando site statico

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.72.0"  # Define a versão aqui
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 5.72.0"  # Define a versão
-  region  = "us-east-1"
+  region = "us-east-1" #Define a região no bloco do provider
 }
