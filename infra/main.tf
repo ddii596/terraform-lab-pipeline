35ef5207a9c6f71e7b6ebfcd0236299474a203d8
ddii596 <adisonph@gmail.com>
2024-10-16 16:31:07 -0300

    testando site statico

--- infra/main.tf
+++ infra/main.tf
@@ -5,8 +5,8 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_object" "object" {
   bucket = var.bucket_name
   key    = "html.index"
-  source = "\infra\index.html"
-  
+  source = "/infra/index.html"
+
 
   
 }
