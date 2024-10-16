402a1d22e0607695d144c6120d92a5b89d498f0d
ddii596 <70442039+ddii596@users.noreply.github.com>
2024-10-16 15:51:03 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,48 +1,14 @@
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket                  = aws_s3_bucket.bucket.id
-  block_public_acls       = false  # Desativa o bloqueio de ACLs públicas
-  block_public_policy     = false  # Desativa o bloqueio de políticas públicas
-  ignore_public_acls      = false  # Permite ACLs públicas
-  restrict_public_buckets = false  # Permite políticas públicas no bucket
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-
-  tags = {
-    Name        = "Static Website Bucket"
-    Environment = "Production"
-  }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_object" "object" {
+  bucket = var.buckrt_name
+  key    = "html.index"
+  source = "html.index"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.bucket.arn}/index.html"  # Permitir acesso apenas ao index.html
-      },
-      {
-        Effect = "Deny"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.bucket.arn}/*"
-        Condition = {
-          StringNotEquals = {
-            "s3:prefix" = "index.html"
-          }
-        }
-      }
-    ]
-  })
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("path/to/file")
 }
