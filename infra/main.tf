0b107cd6982b375c6a6ed3e10763669aed503773
ddii596 <adisonph@gmail.com>
2024-10-16 21:24:16 -0300

    ajustando main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -47,3 +47,18 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     }]
   })
 }
+
+# Política do bucket para permitir acesso público (se necessário)
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.bucket.arn}/*"
+    }]
+  })
+}
