f361af39baade3d661449190e6fe1e8cf25ba50a
ddii596 <adisonph@gmail.com>
2024-10-16 21:04:50 -0300

    ajustando main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,11 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_object" "object" {
+resource "aws_s3_object" "object" {
   bucket = var.bucket_name
   key    = "html.index"
   source = "/home/runner/work/terraform-lab-pipeline/terraform-lab-pipeline/infra/html.index"
+  acl = "public"
 
 
   
