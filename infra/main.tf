12ef5f03cbabd6b5c15d433022d3aeb7d3b75da9
ddii596 <adisonph@gmail.com>
2024-10-17 10:20:41 -0300

    aplicando

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_object" "object" {
   bucket = var.bucket_name
-  key    = "index.html"
+  key    = "html.index"
   source = "/home/runner/work/terraform-lab-pipeline/terraform-lab-pipeline/infra/index.html"
 
 
